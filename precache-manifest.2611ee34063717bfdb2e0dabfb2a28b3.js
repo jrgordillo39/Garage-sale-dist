2191c04c1f5f9ab48e86b25b6801fdfa128c2307
jrgordillo39 <jrgordillo39@gmail.com>
2022-01-08 21:59:21 -0500

    asd

---
+++ precache-manifest.2611ee34063717bfdb2e0dabfb2a28b3.js
@@ -0,0 +1,338 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b68ba315514a2d21751c8eecaf612d0",
+    "url": "/Favicon_AlaCasa 64px.png"
+  },
+  {
+    "revision": "526b2c6b49519861d81313cab1dc47e3",
+    "url": "/Favicon_AlaCasa.svg"
+  },
+  {
+    "revision": "168540f65359be1b3d3d",
+    "url": "/css/app.bc9f1677.css"
+  },
+  {
+    "revision": "0703abcd2ed3c05804c0",
+    "url": "/css/chunk-vendors.f5495fed.css"
+  },
+  {
+    "revision": "5ad343b6e019a48f1c9666843da704db",
+    "url": "/fonts/Barlow-ExtraBold.5ad343b6.ttf"
+  },
+  {
+    "revision": "f653de2d156349ad7b7788a7c24c48c3",
+    "url": "/fonts/Comfortaa-Bold.f653de2d.ttf"
+  },
+  {
+    "revision": "2a61b00fcc9b1afbf99be145777b1b72",
+    "url": "/fonts/MaterialIcons-Regular.2a61b00f.eot"
+  },
+  {
+    "revision": "51cf1d641ae503ece727e7d84b182524",
+    "url": "/fonts/MaterialIcons-Regular.51cf1d64.ttf"
+  },
+  {
+    "revision": "703cf8f274fbb265d49c6262825780e1",
+    "url": "/fonts/MaterialIcons-Regular.703cf8f2.woff2"
+  },
+  {
+    "revision": "84a37de85c17f186652a179b1145392f",
+    "url": "/fonts/MaterialIcons-Regular.84a37de8.woff"
+  },
+  {
+    "revision": "a38a54df0089bca4f5109fe44f944051",
+    "url": "/fonts/Raleway-ExtraBold.a38a54df.ttf"
+  },
+  {
+    "revision": "580d0778ad254335be45bf58bb449f43",
+    "url": "/fonts/Raleway-Regular.580d0778.ttf"
+  },
+  {
+    "revision": "0c9f225e8f69c622f681cf1ed973cc3d",
+    "url": "/fonts/fa-brands-400.0c9f225e.woff2"
+  },
+  {
+    "revision": "0ce1e868452204695c8ac1c70f693c2d",
+    "url": "/fonts/fa-brands-400.0ce1e868.woff"
+  },
+  {
+    "revision": "d28c96cabe5302f1b2c90275cd3f4e69",
+    "url": "/fonts/fa-brands-400.d28c96ca.eot"
+  },
+  {
+    "revision": "dfe5aa4344a2d8a29aec8d83fb3fb14e",
+    "url": "/fonts/fa-brands-400.dfe5aa43.ttf"
+  },
+  {
+    "revision": "0232fe8b09de812c88e6cc27f57a3a88",
+    "url": "/fonts/fa-regular-400.0232fe8b.eot"
+  },
+  {
+    "revision": "398be22b2308585b84da8e858e2d7b60",
+    "url": "/fonts/fa-regular-400.398be22b.ttf"
+  },
+  {
+    "revision": "3cace4a04d941b5981ba32c6ce9afae1",
+    "url": "/fonts/fa-regular-400.3cace4a0.woff"
+  },
+  {
+    "revision": "847712aaabbeba674afdda86d31cab17",
+    "url": "/fonts/fa-regular-400.847712aa.woff2"
+  },
+  {
+    "revision": "00bb62bc33034479d5e82a0fd1c5efe3",
+    "url": "/fonts/fa-solid-900.00bb62bc.eot"
+  },
+  {
+    "revision": "260d033f79f75d670fd09d8cf7c8bf8d",
+    "url": "/fonts/fa-solid-900.260d033f.ttf"
+  },
+  {
+    "revision": "4bfbf7eb4b19d9ff9293eb177b6d0070",
+    "url": "/fonts/fa-solid-900.4bfbf7eb.woff"
+  },
+  {
+    "revision": "9ae050d1876ac1763eb6afe4264e6d5a",
+    "url": "/fonts/fa-solid-900.9ae050d1.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/fonts/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/fonts/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/fonts/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/fonts/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "/fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "/fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "/fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "/fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "0c48cd26e981daa6c28be2304d5ccd62",
+    "url": "/img/0.0c48cd26.jpg"
+  },
+  {
+    "revision": "65a1e0a464db963b13e8b388554798ea",
+    "url": "/img/1.65a1e0a4.jpg"
+  },
+  {
+    "revision": "0fdbaa23b450dbfaee86c78f4460aee6",
+    "url": "/img/2.0fdbaa23.jpg"
+  },
+  {
+    "revision": "90f3ab4033cec146857c69c73d6bfa51",
+    "url": "/img/3.90f3ab40.jpg"
+  },
+  {
+    "revision": "75db5e58de8bd857b1dd91c8971c0cd4",
+    "url": "/img/4.75db5e58.jpg"
+  },
+  {
+    "revision": "4dbbda41a960cc5f85f1c3b048c3f9d5",
+    "url": "/img/5.4dbbda41.jpg"
+  },
+  {
+    "revision": "09304e368858b02dbcd85bdea40e4b7b",
+    "url": "/img/American_Express@2x.09304e36.png"
+  },
+  {
+    "revision": "ea214c7ed1af19ce5b0d1db398588b8f",
+    "url": "/img/Background.ea214c7e.png"
+  },
+  {
+    "revision": "e5bf8c554b9b7a1b3b5c82e001fa7b2c",
+    "url": "/img/Fondo_Footer.e5bf8c55.png"
+  },
+  {
+    "revision": "0b409350bf5c19cba77928ce6c1f0496",
+    "url": "/img/Franja-vertical.0b409350.png"
+  },
+  {
+    "revision": "a174e1acc7afbb1710d0c9f29bc9cd57",
+    "url": "/img/Identidad_AlaCasa.a174e1ac.png"
+  },
+  {
+    "revision": "a174e1acc7afbb1710d0c9f29bc9cd57",
+    "url": "/img/Identidad_AlaCasa.png"
+  },
+  {
+    "revision": "b462ee1a17314de606a1750c9d8792bf",
+    "url": "/img/Identidad_AlaCasa.svg"
+  },
+  {
+    "revision": "44bc9e5396d43e3940b620445057dc2a",
+    "url": "/img/MasterCard@2x.44bc9e53.png"
+  },
+  {
+    "revision": "a6df00f361f87a8960f956b2a2d2e1d3",
+    "url": "/img/Ofertas-Background-3.a6df00f3.png"
+  },
+  {
+    "revision": "5c69ffc061bc9632776db05f59668046",
+    "url": "/img/Ofertas-Background.5c69ffc0.png"
+  },
+  {
+    "revision": "ac5c131e59cb7c75a6ec175cfd538113",
+    "url": "/img/Recargas_if@2x.ac5c131e.png"
+  },
+  {
+    "revision": "60b051acb1dd3bc70bb920e393cf543a",
+    "url": "/img/Visa@2x.60b051ac.png"
+  },
+  {
+    "revision": "3414a55c4abcbf911a2d54afb585f4b2",
+    "url": "/img/casa-en-venta.3414a55c.png"
+  },
+  {
+    "revision": "7a21789bc7ea5e320ff8d339a06b1d14",
+    "url": "/img/combo_mixto.7a21789b.png"
+  },
+  {
+    "revision": "7a21789bc7ea5e320ff8d339a06b1d14",
+    "url": "/img/combo_mixto.png"
+  },
+  {
+    "revision": "e2c3d3b051617ff3f75e0acc1f7528bf",
+    "url": "/img/cooking.png"
+  },
+  {
+    "revision": "0c48cd26e981daa6c28be2304d5ccd62",
+    "url": "/img/cubamax.0c48cd26.jpeg"
+  },
+  {
+    "revision": "0c48cd26e981daa6c28be2304d5ccd62",
+    "url": "/img/cubamax.jpeg"
+  },
+  {
+    "revision": "96f81a8254d1b587e121e28a94e73e0a",
+    "url": "/img/cubamax_logo@2x.96f81a82.png"
+  },
+  {
+    "revision": "1b6c63ee856f08a9f11c9a8adcf468d6",
+    "url": "/img/cubamax_logo_ne@2x.1b6c63ee.png"
+  },
+  {
+    "revision": "a086441df607e8d6587a3ef0c9a9ea0b",
+    "url": "/img/download_app.png"
+  },
+  {
+    "revision": "5d10d00980077f67990f73b1961bc46e",
+    "url": "/img/fa-brands-400.5d10d009.svg"
+  },
+  {
+    "revision": "d0fdc7c20b0b0b1aaf27327547735fa3",
+    "url": "/img/fa-regular-400.d0fdc7c2.svg"
+  },
+  {
+    "revision": "2306f642b31b774ba1d8b6b784539e19",
+    "url": "/img/fa-solid-900.2306f642.svg"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/img/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "71c806428f9d8c76f8dd491ee177382c",
+    "url": "/img/iconfinder_map-marker_299087.71c80642.png"
+  },
+  {
+    "revision": "cdfccca604cb1f7c72048a550bff1dcc",
+    "url": "/img/icons.rar"
+  },
+  {
+    "revision": "e30d0c1b01b7492519a489696c5da1dd",
+    "url": "/img/inicio.e30d0c1b.png"
+  },
+  {
+    "revision": "07d1e22e8cddd99a53037dc510221b7b",
+    "url": "/img/logo.07d1e22e.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "151b615ad33012eff3396ab0c5809ce9",
+    "url": "/img/sunshine.151b615a.jpg"
+  },
+  {
+    "revision": "151b615ad33012eff3396ab0c5809ce9",
+    "url": "/img/sunshine.jpg"
+  },
+  {
+    "revision": "60a34790cfaf5a12bbb94a2d14d42a6d",
+    "url": "/img/venta1.60a34790.jpg"
+  },
+  {
+    "revision": "8bf2909dec60fd7949cb3013394b8dac",
+    "url": "/img/venta11.8bf2909d.png"
+  },
+  {
+    "revision": "af052dca43d58f47a8b4b9b3049c29d7",
+    "url": "/img/venta2.af052dca.jpg"
+  },
+  {
+    "revision": "424e606a0a0deaca094e248be9fd0d75",
+    "url": "/img/venta22.424e606a.png"
+  },
+  {
+    "revision": "6fac33ef7f56954e2551f0b8204d60b1",
+    "url": "/img/venta3.6fac33ef.jpg"
+  },
+  {
+    "revision": "7feb3515753f29187ce49648c10613f9",
+    "url": "/img/venta33.7feb3515.png"
+  },
+  {
+    "revision": "be05f278b2298319e1097d6b0a258bfa",
+    "url": "/img/venta4.be05f278.png"
+  },
+  {
+    "revision": "d0aa8d51edd9d00adebdfd36c62907b7",
+    "url": "/img/venta5.d0aa8d51.png"
+  },
+  {
+    "revision": "3a37af7921e00fd4242d9841addc9b15",
+    "url": "/img/venta6.3a37af79.png"
+  },
+  {
+    "revision": "5d3634321b604f0f55b64fc80e0f2776",
+    "url": "/index.html"
+  },
+  {
+    "revision": "168540f65359be1b3d3d",
+    "url": "/js/app.6cdbf912.js"
+  },
+  {
+    "revision": "0703abcd2ed3c05804c0",
+    "url": "/js/chunk-vendors.4c2afdb7.js"
+  },
+  {
+    "revision": "52718d7fe209abc3964eacc5755ebc34",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
